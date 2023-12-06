4c6068eb2b934a588e7c0ec1ffc43e2b38d9b45f
Terry Lockett <terryjlockett@gmail.com>
2023-12-06 10:36:43 -0400

    2023 Day 06 part 1 & 2

---
+++ src/2023/day06/src/main/kotlin/ca/terrylockett/aoc2023/day06/Day06Runner.kt
@@ -0,0 +1,88 @@
+package ca.terrylockett.aoc2023.day06
+
+import ca.terrylockett.aoccommon.inputfilefinder.InputFileFinder
+import java.io.File
+import java.util.stream.Collectors
+
+class Race(val time: Long, val record: Long)
+
+fun main() {
+    val inputFile: String = InputFileFinder.getInputFilePath("input.txt").orElseThrow()
+
+    println("2023 day06 part1: ${part1(inputFile)}")
+    println("2023 day06 part2: ${part2(inputFile)}")
+}
+
+fun part1(inputFilePath: String): Int {
+    val races = createPart1Races(inputFilePath)
+
+    var total = 1
+    for (race in races) {
+        total *= getNumOfWinningInputs(race)
+    }
+    return total
+}
+
+fun part2(inputFilePath: String): Int {
+    val race = createPart2Race(inputFilePath)
+    return getNumOfWinningInputs(race)
+}
+
+private fun getNumOfWinningInputs(race: Race): Int {
+    var winningInputCount = 0
+    for (timeHoldingButton in 0..race.time) {
+        val distance = timeHoldingButton * (race.time - timeHoldingButton)
+        if (distance > race.record) {
+            winningInputCount++
+        }
+    }
+    return winningInputCount
+}
+
+private fun createPart1Races(inputFilePath: String): List<Race> {
+    val lines = File(inputFilePath).readLines()
+
+    val timeTokens =
+        lines[0]
+            .split(":")[1]
+            .split(Regex("\\s+"))
+            .filter(String::isNotEmpty)
+            .map(String::toLong)
+
+    val recordTokens =
+        lines[1]
+            .split(":")[1]
+            .split(Regex("\\s+"))
+            .filter(String::isNotEmpty)
+            .map(String::toLong)
+
+    val races = ArrayList<Race>()
+
+    for (i in timeTokens.indices) {
+        races.add(Race(timeTokens[i], recordTokens[i]))
+    }
+
+    return races
+}
+
+private fun createPart2Race(inputFilePath: String): Race {
+    val lines = File(inputFilePath).readLines()
+
+    val time =
+        lines[0]
+            .split(":")[1]
+            .split(Regex("\\s+"))
+            .filter(String::isNotEmpty)
+            .stream()
+            .collect(Collectors.joining())
+
+    val record =
+        lines[1]
+            .split(":")[1]
+            .split(Regex("\\s+"))
+            .filter(String::isNotEmpty)
+            .stream()
+            .collect(Collectors.joining())
+
+    return Race(time.toLong(), record.toLong())
+}
---
+++ src/2023/day06/src/test/kotlin/ca/terrylockett/aoc2023/day06/TestDay06.kt
@@ -0,0 +1,21 @@
+package ca.terrylockett.aoc2023.day06
+
+import ca.terrylockett.aoccommon.inputfilefinder.InputFileFinder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestDay06 {
+    @Test
+    fun part1() {
+        val inputFilePath = InputFileFinder.getInputFilePath("test-input.txt").orElseThrow()
+        assertEquals(288, part1(inputFilePath))
+    }
+
+    @Test
+    fun part2() {
+        val inputFilePath = InputFileFinder.getInputFilePath("test-input.txt").orElseThrow()
+        assertEquals(71503, part2(inputFilePath))
+    }
+}
