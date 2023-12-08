6ebd60bdc67c6dc4b293a25d68a2d3a61bcebd34
Terry Lockett <terryjlockett@gmail.com>
2023-12-07 22:39:27 -0400

    no more groovy!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,9 @@ fun includeSubprojects(dirName: String) {
     val file = File("$rootDir/src/$dirName")
 
     file.walk().filter { it.isDirectory }.forEach { subDir ->
-        val groovyFile = File(subDir.path, "build.gradle")
         val kotlinFile = File(subDir.path, "build.gradle.kts")
-        
-        if (groovyFile.exists() || kotlinFile.exists()) {
+
+        if (kotlinFile.exists()) {
             include("$dirName:${subDir.name}")
             project(":$dirName:${subDir.name}").projectDir = File("$rootDir/src/$dirName/${subDir.name}")
         }
