0c1ec6dc51eb0f0b66bfa1ef49a772e4a838400c
Terry Lockett <5551818+terrylockett@users.noreply.github.com>
2025-12-02 16:09:56 -0400

    2025 day02 part2

--- src/2025/day02/src/main/java/ca/terrylockett/aoc2025/day02/Day02Runner.java
+++ src/2025/day02/src/main/java/ca/terrylockett/aoc2025/day02/Day02Runner.java
@@ -2,24 +2,36 @@ package ca.terrylockett.aoc2025.day02;
 
 import ca.terrylockett.aoccommon.resources.Resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day02Runner {
 
 	static final String INPUT_FILE_NAME = "input.txt";
+	static final int DIVISOR_CACHE_SIZE = 10;
+
+	static final Map<Integer, Pattern> DIVISOR_PATTERNS = new HashMap<>();
+	static final Map<Integer, Set<Integer>> WHOLE_DIVISORS = new HashMap<>();
+	static {
+		for (int i = 1; i <= DIVISOR_CACHE_SIZE; i++) {
+			DIVISOR_PATTERNS.put(i, Pattern.compile(String.format("(\\w{%d})", i)));
+			WHOLE_DIVISORS.put(i, getWholeDivisors(i));
+		}
+	}
 
 	public static void main(String[] args) {
 		String input = Resources.getInput(INPUT_FILE_NAME).orElseThrow();
+		List<Range> ranges = Range.createRanges(input);
 
-		System.out.println("2025 day02 part1: " + part1(input));
-		// System.out.println("2025 day02 part2: " + part2(input));
+		System.out.println("2025 day02 part1: " + part1(ranges));
+		System.out.println("2025 day02 part2: " + part2(ranges));
 	}
 
-	static long part1(String input) {
+	static long part1(List<Range> ranges) {
 		List<Long> invalidIds = new ArrayList<>();
 
-		for (Range range : Range.createRanges(input)) {
+		for (Range range : ranges) {
 			for (long i = range.getStart(); i <= range.getEnd(); i++) {
 				String productId = String.valueOf(i);
 				if (productId.length() % 2 != 0) {
@@ -34,7 +46,54 @@ public class Day02Runner {
 				}
 			}
 		}
+		return invalidIds.stream().mapToLong(Long::longValue).sum();
+	}
+
+	static long part2(List<Range> ranges) {
+		List<Long> invalidIds = new ArrayList<>();
+
+		for (Range range : ranges) {
+			for (long i = range.getStart(); i <= range.getEnd(); i++) {
+				if (isInvalidProductIdPt2(i)) {
+					invalidIds.add(i);
+				}
+			}
+		}
 
 		return invalidIds.stream().mapToLong(Long::longValue).sum();
+	}
+
+	static boolean isInvalidProductIdPt2(long id) {
+		String productId = String.valueOf(id);
+		if (productId.length() == 1) {
+			return false;
+		}
+		Set<Integer> wholeDivisors = WHOLE_DIVISORS.get(productId.length());
+
+		for (Integer divisor : wholeDivisors) {
+			Matcher m = DIVISOR_PATTERNS.get(divisor).matcher(productId);
+
+			Set<String> groups = new HashSet<>();
+			while (m.find()) {
+				groups.add(m.group(1));
+				if (groups.size() > 1) {
+					break;
+				}
+			}
+			if (groups.size() == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static Set<Integer> getWholeDivisors(int stringLength) {
+		Set<Integer> wholeDivisors = new HashSet<>(Set.of(1));
+		for (int i = 2; i <= stringLength / 2; i++) {
+			if (stringLength % i == 0) {
+				wholeDivisors.add(i);
+			}
+		}
+		return wholeDivisors;
 	}
 }
--- src/2025/day02/src/test/java/ca/terrylockett/aoc2025/day02/TestDay02.java
+++ src/2025/day02/src/test/java/ca/terrylockett/aoc2025/day02/TestDay02.java
@@ -4,27 +4,47 @@ import ca.terrylockett.aoccommon.resources.Resources;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TestDay02 {
 
 	static final String TEST_FILE_NAME = "test-input.txt";
 
 	static String testInput = "";
+	static List<Range> ranges;
 
 	@BeforeAll
 	static void setup() {
 		testInput = Resources.getInput(TEST_FILE_NAME).orElseThrow();
+		ranges = Range.createRanges(testInput);
 	}
 
 	@Test
 	void testDay02part1() {
-		assertEquals(1227775554L, Day02Runner.part1(testInput));
+		assertEquals(1227775554L, Day02Runner.part1(ranges));
+	}
+
+	@Test
+	void testDay02part2() {
+		assertEquals(4174379265L, Day02Runner.part2(ranges));
+	}
+
+	@Test
+	void testIsInvalidIdPt2() {
+		assertTrue(Day02Runner.isInvalidProductIdPt2(12341234));
+		assertTrue(Day02Runner.isInvalidProductIdPt2(11));
+		assertTrue(Day02Runner.isInvalidProductIdPt2(999));
+		assertTrue(Day02Runner.isInvalidProductIdPt2(123123123));
+
+		assertFalse(Day02Runner.isInvalidProductIdPt2(12));
+		assertFalse(Day02Runner.isInvalidProductIdPt2(1231234));
+		assertFalse(Day02Runner.isInvalidProductIdPt2(5));
 	}
 
 	@Test
 	void testRangeCreation() {
-		var ranges = Range.createRanges(testInput);
 		assertEquals(11, ranges.size());
 		// First Range
 		assertEquals(11L, ranges.get(0).getStart());
